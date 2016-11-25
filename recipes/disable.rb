48cbc50d622b851623fecbfb960780f69e78470e
Tim Smith <tsmith@chef.io>
2016-11-25 15:02:11 -0800

    Update copyright headers
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/disable.rb
+++ recipes/disable.rb
@@ -1,9 +1,9 @@
 #
 # Author:: Matt Ray <matt@chef.io>
-# Cookbook Name:: ufw
+# Cookbook:: ufw
 # Recipe:: disable
 #
-# Copyright 2011-2016, Chef Software, Inc.
+# Copyright:: 2011-2016, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
