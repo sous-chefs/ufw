6cb82919c6b44d4843a08c0ee1fd73fcbc7f48d4
Tim Smith <tsmith@chef.io>
2017-02-12 06:57:32 -0800

    Testing updates
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -11,4 +11,4 @@ supports 'ubuntu'
 
 source_url 'https://github.com/chef-cookbooks/ufw'
 issues_url 'https://github.com/chef-cookbooks/ufw/issues'
-chef_version '>= 12.1'
+chef_version '>= 12.1' if respond_to?(:chef_version)
