4315021e420659505775474aece5a6ca9a7ef598
Tim Smith <tsmith@chef.io>
2017-03-21 16:00:58 -0700

    Update apache2 license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'ufw'
 maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs and configures Uncomplicated Firewall (ufw)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '3.1.0'
