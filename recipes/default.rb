8ae14198dc4c0c5a46487261979a7905ed049c9c
Austin Heiman <atheimanksu@gmail.com>
2017-12-18 14:52:26 -0600

    allow rules attribute to be specified as Hash
    Signed-off-by: Austin Heiman <atheimanksu@gmail.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -29,43 +29,52 @@ firewall_rule 'ssh' do
   only_if { node['firewall']['allow_ssh'] }
 end
 
-node['firewall']['rules'].each do |rule_mash|
-  Chef::Log.debug "ufw:rule \"#{rule_mash}\""
-  rule_mash.keys.each do |rule|
-    Chef::Log.debug "ufw:rule:name \"#{rule}\""
-    params = rule_mash[rule]
-    Chef::Log.debug "ufw:rule:parameters \"#{params}\""
-    Chef::Log.debug "ufw:rule:name #{params['name']}" if params['name']
-    Chef::Log.debug "ufw:rule:protocol #{params['protocol']}" if params['protocol']
-    Chef::Log.debug "ufw:rule:direction #{params['direction']}" if params['direction']
-    Chef::Log.debug "ufw:rule:interface #{params['interface']}" if params['interface']
-    Chef::Log.debug "ufw:rule:logging #{params['logging']}" if params['logging']
-    Chef::Log.debug "ufw:rule:port #{params['port']}" if params['port']
-    Chef::Log.debug "ufw:rule:port_range #{params['port_range']}" if params['port_range']
-    Chef::Log.debug "ufw:rule:source #{params['source']}" if params['source']
-    Chef::Log.debug "ufw:rule:destination #{params['destination']}" if params['destination']
-    Chef::Log.debug "ufw:rule:dest_port #{params['dest_port']}" if params['dest_port']
-    Chef::Log.debug "ufw:rule:position #{params['position']}" if params['position']
-    act = params['action']
-    act ||= 'create'
-    raise 'ufw: port_range was specified to firewall_rule without protocol' if params['port_range'] && !params['protocol']
-    Chef::Log.debug "ufw:rule:action :#{act}"
-    firewall_rule rule do
-      name params['name'] if params['name']
-      protocol params['protocol'].to_sym if params['protocol']
-      direction params['direction'].to_sym if params['direction']
-      interface params['interface'] if params['interface']
-      logging params['logging'].to_sym if params['logging']
-      port params['port'].to_i if params['port']
-      if params['port_range']
-        ends = params['port_range'].split('..').map { |d| Integer(d) }
-        port_range ends[0]..ends[1]
-      end
-      source params['source'] if params['source']
-      destination params['destination'] if params['destination']
-      dest_port params['dest_port'].to_i if params['dest_port']
-      position params['position'].to_i if params['position']
-      action act
+# handle rules specified as array or as hash
+rules = {}
+if node['firewall']['rules'].is_a?(Array)
+  node['firewall']['rules'].each do |rule_mash|
+    rule_mash.keys.each do |rule|
+      rules[rule] = rule_mash[rule]
     end
+  end
+elsif node['firewall']['rules'].is_a?(Hash)
+  rules = node['firewall']['rules']
+end
+
+rules.keys.each do |rule|
+  Chef::Log.debug "ufw:rule:name \"#{rule}\""
+  params = rules[rule]
+  Chef::Log.debug "ufw:rule:parameters \"#{params}\""
+  Chef::Log.debug "ufw:rule:name #{params['name']}" if params['name']
+  Chef::Log.debug "ufw:rule:protocol #{params['protocol']}" if params['protocol']
+  Chef::Log.debug "ufw:rule:direction #{params['direction']}" if params['direction']
+  Chef::Log.debug "ufw:rule:interface #{params['interface']}" if params['interface']
+  Chef::Log.debug "ufw:rule:logging #{params['logging']}" if params['logging']
+  Chef::Log.debug "ufw:rule:port #{params['port']}" if params['port']
+  Chef::Log.debug "ufw:rule:port_range #{params['port_range']}" if params['port_range']
+  Chef::Log.debug "ufw:rule:source #{params['source']}" if params['source']
+  Chef::Log.debug "ufw:rule:destination #{params['destination']}" if params['destination']
+  Chef::Log.debug "ufw:rule:dest_port #{params['dest_port']}" if params['dest_port']
+  Chef::Log.debug "ufw:rule:position #{params['position']}" if params['position']
+  act = params['action']
+  act ||= 'create'
+  raise 'ufw: port_range was specified to firewall_rule without protocol' if params['port_range'] && !params['protocol']
+  Chef::Log.debug "ufw:rule:action :#{act}"
+  firewall_rule rule do
+    name params['name'] if params['name']
+    protocol params['protocol'].to_sym if params['protocol']
+    direction params['direction'].to_sym if params['direction']
+    interface params['interface'] if params['interface']
+    logging params['logging'].to_sym if params['logging']
+    port params['port'].to_i if params['port']
+    if params['port_range']
+      ends = params['port_range'].split('..').map { |d| Integer(d) }
+      port_range ends[0]..ends[1]
+    end
+    source params['source'] if params['source']
+    destination params['destination'] if params['destination']
+    dest_port params['dest_port'].to_i if params['dest_port']
+    position params['position'].to_i if params['position']
+    action act
   end
 end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,25 +1,45 @@
 require 'spec_helper'
 
 describe 'ufw::default' do
-  cached(:chef_run) do
-    ChefSpec::ServerRunner.new do |node, _server|
-      node.normal['firewall']['rules'] = [
-        { 'http'  => { 'port' => '80'  } },
-      ]
-      node.override['firewall']['rules'] = [
-        { 'https' => { 'port' => '443' } },
-      ]
-    end.converge(described_recipe)
+  context 'rules attribute is Array' do
+    cached(:chef_run) do
+      ChefSpec::ServerRunner.new do |node, _server|
+        node.normal['firewall']['rules'] = [
+          { 'http'  => { 'port' => '80'  } },
+          { 'https' => { 'port' => '443' } },
+        ]
+      end.converge(described_recipe)
+    end
+
+    it 'calls firewall rule for each ' do
+      expect(chef_run).to create_firewall_rule('http').with(
+        port: 80,
+        protocol: :tcp
+      )
+      expect(chef_run).to create_firewall_rule('https').with(
+        port: 443,
+        protocol: :tcp
+      )
+    end
   end
 
-  it 'calls firewall rule for each ' do
-    expect(chef_run).to create_firewall_rule('http').with(
-      port: 80,
-      protocol: :tcp
-    )
-    expect(chef_run).to create_firewall_rule('https').with(
-      port: 443,
-      protocol: :tcp
-    )
+  context 'rules attribute is Hash' do
+    cached(:chef_run) do
+      ChefSpec::ServerRunner.new do |node, _server|
+        node.normal['firewall']['rules']   = { 'http'  => { 'port' => '80'  } }
+        node.override['firewall']['rules'] = { 'https' => { 'port' => '443' } }
+      end.converge(described_recipe)
+    end
+
+    it 'calls firewall rule for each ' do
+      expect(chef_run).to create_firewall_rule('http').with(
+        port: 80,
+        protocol: :tcp
+      )
+      expect(chef_run).to create_firewall_rule('https').with(
+        port: 443,
+        protocol: :tcp
+      )
+    end
   end
 end
